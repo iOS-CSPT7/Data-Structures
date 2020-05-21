1bdc1d42ecafb2cab21775e81b2c3ebeb385ac7c
Stephanie <sbowl001@fiu.edu>
2020-05-21 10:22:41 +0800

    doubly linked passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -76,7 +76,7 @@ class DoublyLinkedList:
         value = self.head.value
         self.delete(self.head)
         return value 
-        pass
+        # pass
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -100,24 +100,33 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value 
+        self.delete(self.head)
+        return value 
+        # pass
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-
+        # self.delete(node)
+        # node.next = self.head
+        # self.head.prev = node
+        # self.head = node
+        if node is self.head:
+            return 
+        value = node.value 
         self.delete(node)
-    #  CHECK THIS again on lecture. TODO
-        node.next = self.head 
-        self.head.prev = node
-        self.head = node 
-        self.add_to_head(node)
-        pass
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail: 
+            return 
+        value = node.value 
+        self.delete(node)
+        self.add_to_tail(value)
+        # pass
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -152,4 +161,12 @@ class DoublyLinkedList:
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        value = self.head.value 
+        current = self.head
+
+        while current is not None:
+            if current.value > value:
+                value = current.value 
+            current = current.next
+        return value 
+  
